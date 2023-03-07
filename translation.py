4d5d606d4ae544fdf5474cbf027a2e6e6c1585f3
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-03-07 15:30:31 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -87,7 +87,7 @@ You can use /rename command after receiving file to rename it with custom thumbn
 ◽ Channel : @knoxprojects
 ◽ Support  : @knoxsupport
 ◽ Source : [Ace](https://github.com/aceknox)
-◽ Server  : [Railway](https://railway.app/)
+◽ Server  : [Heroku](https://heroku.com)
 ◽ Language : [Python 3.10.5](https://www.python.org/)
 ◽ Framework  : [Pyrogram 1.8.30](https://docs.pyrogram.org/)
 ◽ Developer  : @aceknox
